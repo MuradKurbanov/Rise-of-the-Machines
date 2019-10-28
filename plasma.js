df067c5f21d495d333b3f9809bf10d08339667fe
kurbanov <mkurbanov@kplatform.ru>
2019-10-29 01:52:41 +0300

    add fire control

--- plasma.js
+++ plasma.js
@@ -1,25 +1,28 @@
 class Plasma {
   constructor(params) {
-    this.heroY = params.heroY;
-    this.plasmaX = 200;
+    this.plasmaX = 300;
+    this.plasmaY = params.heroY + 50;
     this.render();
   }
-
   render = () => {
     const plasmaNode = document.createElement('div');
     plasmaNode.id = 'plasma';
+    plasmaNode.style.top = `${this.plasmaY}px`;
+    plasmaNode.style.left = `${this.plasmaX}px`;
     wrapper.append(plasmaNode);
-    plasmaNode.style.top = `${this.heroY + 50}px`;
-    plasmaNode.style.left = `${this.heroX + 50}px`;
     const createPlasma = setInterval(() => {
       if (this.plasmaX <= 1000) {
-        this.plasmaX += 100;
-        plasmaNode.style.top = `${this.heroY + 50}px`;
+        this.plasmaX += 50;
         plasmaNode.style.left = `${this.plasmaX}px`;
+        plasmaNode.style.top = `${this.plasmaY}px`;
+        plasmaNode.setAttribute('x', this.plasmaX);
+        plasmaNode.setAttribute('y', this.plasmaY);
+        coordinates.add(plasmaNode);
       } else {
         plasmaNode.remove();
         clearInterval(createPlasma);
+        coordinates.delete(plasmaNode);
       }
-    }, 200)
+    }, 300);
   }
 }
