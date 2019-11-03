db4505adb1ff938e9a459b9502b887e677afb2f6
kurbanov <mkurbanov@kplatform.ru>
2019-11-04 01:54:13 +0300

    added determination of the coordinates of the hero for the rocket and the consequences of hitting the hero with the rocket

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const wrapper = document.getElementById('wrapper');
-const confirm = (a, b) => a - b < 120 && a > b || b - a < 120 && b > a;
+const heroNode = document.getElementById('hero');
+const confirm = (a, b) => a - b < 10 && a > b || b - a < 100 && b > a;
 const random = (min, max) => Math.round(min + Math.random() * (max - min + 1));
 const coordinates = new Set();
+const coordinatesHero = new Set();
+let isEinfrierenHero = false;
 
 class Node {
   constructor(params) {
@@ -17,21 +20,41 @@ class Node {
     node.id = `${this.style}`;
     node.style.top = `${this.coordY}px`;
     wrapper.append(node);
-    coordinates.add(node);
     const moveNode = setInterval(() => {
-      Array.from(coordinates).forEach((item, i, arr) => {
-        let n1 = item.getBoundingClientRect()
-        let n2 = node.getBoundingClientRect();
-        if (confirm(n1.y, n2.y) && confirm(n1.x, n2.x) && this.node.id !== 'rocket') {
-          coordinates.delete(node);
-          coordinates.delete(item);
-          node.remove();
-          item.remove();
-          clearInterval(moveNode);
-        }
-      })
-      if (this.style === 'robot' || this.style === 'rocket' && this.node.getBoundingClientRect().x > 55) node.style.left = '50px';
+      if  (this.style === 'rocket') {
+        coordinatesHero.add(node);
+        Array.from(coordinatesHero).forEach((item, i, arr) => {
+          let n1 = item.getBoundingClientRect()
+          let n2 = node.getBoundingClientRect();
+          if (confirm(n1.y, n2.y) && confirm(n1.x, n2.x)) {
+            heroNode.className = "blinkHero";
+            isEinfrierenHero = true;
+            setTimeout(() => {
+              isEinfrierenHero = false;
+              heroNode.className = "";
+            }, 3000)
+            coordinates.delete(node);
+            node.remove();
+            clearInterval(moveNode);
+          }
+        })
+      } else {
+        coordinates.add(node);
+        Array.from(coordinates).forEach((item, i, arr) => {
+          let n1 = item.getBoundingClientRect()
+          let n2 = node.getBoundingClientRect();
+          if (confirm(n1.y, n2.y) && confirm(n1.x, n2.x)) {
+            coordinates.delete(node);
+            coordinates.delete(item);
+            node.remove();
+            item.remove();
+            clearInterval(moveNode);
+          }
+        })
+      }
+      if (this.style === 'robot' && this.node.getBoundingClientRect().x > 55) node.style.left = '50px';
       else if (this.style === 'plasma' && this.node.getBoundingClientRect().x < 1050) node.style.left = '1100px';
+      else if (this.style === 'rocket' && this.node.getBoundingClientRect().x > 55) node.style.left = '50px';
       else { node.remove(); coordinates.delete(node); clearInterval(moveNode); }
     }, 400);
   }
@@ -39,19 +62,24 @@ class Node {
 
 const heroInAction = () => {
   const HeightScreen = document.documentElement.clientHeight - 150;
-  const heroNode = document.getElementById('hero');
   let coordY = 400;
+  coordinatesHero.add(heroNode);
   wrapper.onmousemove = (e) => {
-    coordY = e.clientY;
-    heroNode.style.top = `${coordY <= HeightScreen ? coordY : null}px`;
+    if (!isEinfrierenHero) {
+      coordinatesHero.add(heroNode);
+      coordY = e.clientY;
+      heroNode.style.top = `${coordY <= HeightScreen ? coordY : null}px`;
+    }
   }
 
   // Create Plasma
-  setInterval(() => new Node({
-    coordX: 300,
-    coordY: coordY + 55,
-    style: 'plasma',
-  }),1500);
+  setInterval(() => (
+    !isEinfrierenHero && new Node({
+      coordX: 110,
+      coordY: coordY + 40,
+      style: 'plasma',
+    })
+  ),1500);
 }
 heroInAction();
 
